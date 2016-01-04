4e0de59b3af18715e2836fae91aff074b19c1d65
chervand <chervand@gmail.com>
2016-01-04 13:15:00 +0100

    nav relations updated

--- models/Nav.php
+++ models/Nav.php
@@ -1,11 +1,19 @@
 <?php
-/**
- */
 
 namespace chervand\nav\models;
 
 use yii\db\ActiveRecord;
+use yii\db\ActiveQuery;
 
+/**
+ * Class Nav
+ * @package chervand\nav\models
+ *
+ * @property Assignment[] $assignments
+ * @property ItemChild[] $itemsJunctions
+ * @property Item[] $items
+ * @property array $itemsAsArray
+ */
 class Nav extends ActiveRecord
 {
     /**
@@ -21,8 +29,22 @@ class Nav extends ActiveRecord
         return Nav::find()->one();
     }
 
-    public function getItems()
+    public function getItemsJunctions()
     {
         return $this->hasMany(ItemChild::className(), ['nav_id' => 'id']);
+    }
+
+    public function getItems()
+    {
+        return $this->hasMany(Item::className(), ['name' => 'child_name'])
+            ->with('childItems')
+            ->via('itemsJunctions', function (ActiveQuery $query) {
+                $query->andWhere(['parent_name' => 'root']);
+            });
+    }
+
+    public function getItemsAsArray()
+    {
+        return $this->getItems()->asArray();
     }
 }
